246c84c38368ce1842064b119e3bc90171b40120
Sergej Maul <maulser@gmail.com>
2025-03-20 22:29:09 +0100

    added script to install GNOME shell extensions

---
+++ Linux/bash/extensions.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt upgrade
+
+# manual search for extensions
+#sudo apt-get search gnome-shell-extension
+
+sudo apt install -y \
+gnome-shell-extension-manager \
+gnome-shell-extension-alphabetical-grid \
+gnome-shell-extension-appindicator \
+gnome-shell-extension-gpaste \
+gnome-shell-extensions \
+variety
+
+# Additional extensions website
+# ensure browser extension is installed
+# https://extensions.gnome.org/extension/3780/ddterm/
+# https://extensions.gnome.org/extension/1460/vitals/
